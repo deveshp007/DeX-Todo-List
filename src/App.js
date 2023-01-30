8edca90278c789bb7c3029cc3e0a332adbc245e2
Devesh Pandey <72290650+deveshp007@users.noreply.github.com>
2023-01-30 19:14:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css";
 
-function App() {
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    if (!input) {
+      alert('Please type something to add !!');
+      return;
+    }
+    setTodos([...todos, input]);
+    setInput('');
+  };
+
+  const removeTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
+  const element = "DeX Todo List";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div className='heading'>
+      <h1 > {element} </h1>
+      <form onSubmit={addTodo} className='form'>
+        <input
+          type="text"
+          placeholder='Enter Todo Item Detail'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit" className='addbtn'>Add Todo</button>
+      </form>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => removeTodo(index)} className='remove'>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default TodoList;
